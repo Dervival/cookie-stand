46bbf92c60fa01694c586e746ea2614515ba4aa9
Blaise Clarke <blaiserclarke@gmail.com>
2018-09-18 13:16:26 -0700

    Fixed off by one error in RNG; refactored yesterday's code to constructor patterning

--- app.js
+++ app.js
@@ -1,88 +1,48 @@
 'use strict';
 var hoursOpen = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 var storeNames = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
+var minCustArray  = [23, 3, 11, 20, 2];
+var maxCustArray  = [65, 24, 38, 38, 16];
+var avgSaleArray = [6.3, 1.2, 3.7, 2.3, 4.6];
 
-var pikeStore = {
-  //Hardcoded properties
-  storeName: storeNames[0],
-  minCust: 23,
-  maxCust: 65,
-  avgSale: 6.3,
-  //Generated properties
-  salesData: [],
-  totalSales: 0,
-  //Methods
-  generateSales: generateSalesData,
-  writeData: writeToPage
+var Store = function(storeName, minCust, maxCust, avgSale){
+  //hard coded constructor properties
+  this.storeName = storeName;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgSale = avgSale;
+  //bases of generated properties
+  this.custData = [];
+  this.salesData = [];
+  this.totalCust = 0;
+  this.totalSales = 0;
+  Store.locations.push(this);
 };
 
-var seatacStore = {
-  storeName: storeNames[1],
-  minCust: 3,
-  maxCust: 24,
-  avgSale: 1.2,
-  salesData: [],
-  totalSales: 0,
-  generateSales: generateSalesData,
-  writeData: writeToPage
+Store.locations = [];
+Store.totalCust = 0;
+Store.totalSales = 0;
+
+Store.prototype.generateCustomerData = function(){
+  let custFloor = this.minCust;
+  let custRange = this.maxCust - custFloor + 1; //previous off by one error - was non-inclusive of max
+  return Math.floor((custFloor + ( custRange*Math.random() ) ));
 };
 
-var seatCentStore = {
-  storeName: storeNames[2],
-  minCust: 11,
-  maxCust: 38,
-  avgSale: 3.7,
-  salesData: [],
-  totalSales: 0,
-  generateSales: generateSalesData,
-  writeData: writeToPage
-};
-
-var capHillStore = {
-  storeName: storeNames[3],
-  minCust: 20,
-  maxCust: 38,
-  avgSale: 2.3,
-  salesData: [],
-  totalSales: 0,
-  generateSales: generateSalesData,
-  writeData: writeToPage
-};
-
-var alkiStore = {
-  storeName: storeNames[4],
-  minCust: 2,
-  maxCust: 16,
-  avgSale: 4.6,
-  salesData: [],
-  totalSales: 0,
-  generateSales: generateSalesData,
-  writeData: writeToPage
-};
-
-function generateSalesData() {
+Store.prototype.generateSalesData = function(){
   console.log(`Generating sales data for ${this.storeName}`);
   //let debugAccum = 0;
   for(var i = 0; i < hoursOpen.length; i++){
-    let custFloor = this.minCust;
-    let custRange = this.maxCust - custFloor;
-    let hourlySales = Math.floor((custFloor + ( custRange*Math.random() ) )*this.avgSale);
-    console.log(`Sales for ${hoursOpen[i]}: ${hourlySales}`);
+    let hourlyCust = this.generateCustomerData();
+    this.custData.push(hourlyCust);
+    this.totalCust += hourlyCust;
+    let hourlySales = Math.floor(hourlyCust*this.avgSale);
     this.salesData.push(hourlySales);
-    //debugAccum += hourlySales;
+    this.totalSales += hourlySales;
   }
-  //console.log(debugAccum + ' sales for the day');
-}
+};
 
-function generateTotalSales(salesArray) {
-  let salesAccum = 0;
-  for(var i = 0; i < salesArray.length; i++){
-    salesAccum += salesArray[i];
-  }
-  return salesAccum;
-}
-
-function writeToPage(){
+Store.prototype.writeToPage = function(){
   var headerNode = document.createElement('p');
   headerNode.id = this.storeName;
   var headerText = document.createTextNode(this.storeName);
@@ -94,7 +54,8 @@ function writeToPage(){
     appendUlElement(hoursOpen[i], this.salesData[i], unordListNode);
   }
   appendUlElement('Total', this.totalSales, unordListNode);
-}
+};
+
 function appendUlElement(hour, cookies, nodeHead){
   let unordListEl = document.createElement('li');
   let unordListElText = `${hour}: ${cookies} cookies`;
@@ -102,22 +63,18 @@ function appendUlElement(hour, cookies, nodeHead){
   nodeHead.appendChild(unordListEl);
 }
 
-pikeStore.generateSales();
-pikeStore.totalSales = generateTotalSales(pikeStore.salesData);
-pikeStore.writeData();
-
-seatacStore.generateSales();
-seatacStore.totalSales = generateTotalSales(seatacStore.salesData);
-seatacStore.writeData();
-
-seatCentStore.generateSales();
-seatCentStore.totalSales = generateTotalSales(seatCentStore.salesData);
-seatCentStore.writeData();
-
-capHillStore.generateSales();
-capHillStore.totalSales = generateTotalSales(capHillStore.salesData);
-capHillStore.writeData();
-
-alkiStore.generateSales();
-alkiStore.totalSales = generateTotalSales(alkiStore.salesData);
-alkiStore.writeData();
+var pikeStore = new Store(storeNames[0],minCustArray[0],maxCustArray[0],avgSaleArray[0]);
+var seatacStore = new Store(storeNames[1],minCustArray[1],maxCustArray[1],avgSaleArray[1]);
+var seatCentStore = new Store(storeNames[2],minCustArray[2],maxCustArray[2],avgSaleArray[2]);
+var capHillStore = new Store(storeNames[3],minCustArray[3],maxCustArray[3],avgSaleArray[3]);
+var alkiStore = new Store(storeNames[4],minCustArray[4],maxCustArray[4],avgSaleArray[4]);
+for(var i = 0; i < Store.locations.length; i++){
+  Store.locations[i].generateCustomerData();
+  Store.locations[i].generateSalesData();
+  Store.locations[i].writeToPage();
+}
+console.log(pikeStore);
+console.log(seatacStore);
+console.log(seatCentStore);
+console.log(capHillStore);
+console.log(alkiStore);
